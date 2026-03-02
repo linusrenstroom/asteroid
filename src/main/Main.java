f663fd5e349018191ab203c3f2ac4b26789502f9
23emli04 <emilmoff@hotmail.com>
2026-03-02 10:05:26 +0100

    .

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+        this.setSize(800, 600);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
